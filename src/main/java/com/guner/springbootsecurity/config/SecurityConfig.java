e22aaab5174bebb5ab00d307b9f0bf77658b6613
Ramazan Guner <ramazan.guner@haud.com>
2023-10-27 18:05:03 +0300

    first commit

---
+++ src/main/java/com/guner/springbootsecurity/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.guner.springbootsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	/* to disable security
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(
+			authorize -> authorize.anyRequest().permitAll());
+		return http.build();
+	}
+
+	 */
+}
