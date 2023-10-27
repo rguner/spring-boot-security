6cea67ca133fc860f751e81c38d0fbf129c4e50a
Ramazan Guner <ramazan.guner@haud.com>
2023-10-27 18:23:09 +0300

    form login page

--- src/main/java/com/guner/springbootsecurity/config/SecurityConfig.java
+++ src/main/java/com/guner/springbootsecurity/config/SecurityConfig.java
@@ -3,10 +3,35 @@ package com.guner.springbootsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(
+                        authorize -> authorize.anyRequest().authenticated())
+                 .formLogin(); // to enable form login html page
+        ;
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("javainuse").password("javainuse").authorities("read").build();
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
 
 	/* to disable security
 	@Bean
@@ -15,6 +40,6 @@ public class SecurityConfig {
 			authorize -> authorize.anyRequest().permitAll());
 		return http.build();
 	}
-
 	 */
+
 }
